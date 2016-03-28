c4b184b1d2090460294ac07b07bacf9e12cbad88
Kristofer Bergh <bergh86@zoho.com>
2016-03-28 21:08:12 +0800

    Changed screen to move

--- ButtonSwipe/ViewController.m
+++ ButtonSwipe/ViewController.m
@@ -10,7 +10,8 @@
 
 @interface ViewController ()
 {
-    IBOutlet UIButton*  button;
+    UIButton*       button;
+    IBOutlet UIScrollView* scrollView;
 }
 
 @end
@@ -20,6 +21,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect rc = self.view.frame;
+    CGSize buttonSize = CGSizeMake(150, 40);
+    
+    button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0,
+                               buttonSize.width,
+                               buttonSize.height)];
+    [button setBackgroundColor:[UIColor blueColor]];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setTitle:@"Button" forState:UIControlStateNormal];
+    
+    [scrollView addSubview:button];
+    [scrollView setContentSize:CGSizeMake(rc.size.width * 2, rc.size.height * 2)];
+}
+
+-(void) viewDidLayoutSubviews
+{
+    CGRect rcView = scrollView.frame;
+    CGRect rcBtn = button.frame;
+    CGSize contentSize = CGSizeMake(rcView.size.width * 2 - rcBtn.size.width, rcView.size.height * 2 - rcBtn.size.height);
+    [scrollView setContentSize:contentSize];
+    
+    rcBtn.origin = CGPointMake(contentSize.width / 2 - rcBtn.size.width / 2,
+                                     contentSize.height / 2 - rcBtn.size.height / 2);
+    button.frame = rcBtn;
+    
+    [scrollView setContentOffset:CGPointMake(contentSize.width / 4 - rcBtn.size.width / 4,
+                                            contentSize.height / 4 - rcBtn.size.height / 4)];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,23 +56,4 @@
     // Dispose of any resources that can be recreated.
 }
 
--(IBAction) displayGestureForSwipeRecognize:(UISwipeGestureRecognizer*) recognizer
-{
-    __block CGRect rc = button.frame;
-    
-    switch (recognizer.direction) {
-        case UISwipeGestureRecognizerDirectionUp:
-            rc.origin.y = 0;
-            break;
-        case UISwipeGestureRecognizerDirectionDown:
-            rc.origin.y = self.view.frame.size.height - rc.size.height;
-            break;
-        default:
-            return;
-    }
-
-    [UIView animateWithDuration:0.5f animations:^{
-        button.frame = rc;
-    }];
-}
 @end
